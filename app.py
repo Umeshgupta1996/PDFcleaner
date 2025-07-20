273c5650407604064511afe0cc3a3ec3daa26e68
UMESH\Umesh Gupta <umeshgupta97162@gmail.com>
2025-07-20 18:43:55 +0530

    change port

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def download_file(filename):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
